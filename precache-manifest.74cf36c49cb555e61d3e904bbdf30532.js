0c4e21cbc1966c825abaaf1b46727966e283ff12
Phillip Tran <pntran@ucdavis.edu>
2020-06-07 12:10:23 -0700

    Updates

---
+++ precache-manifest.74cf36c49cb555e61d3e904bbdf30532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899eac6d71ef5f9b0c4c6587f78aadd1",
+    "url": "/DegenChat/index.html"
+  },
+  {
+    "revision": "f07e4d55269416340bd8",
+    "url": "/DegenChat/static/css/main.553046b2.chunk.css"
+  },
+  {
+    "revision": "4554ceeac349b065ab5a",
+    "url": "/DegenChat/static/js/2.27e3ecdb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/DegenChat/static/js/2.27e3ecdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07e4d55269416340bd8",
+    "url": "/DegenChat/static/js/main.fafc3847.chunk.js"
+  },
+  {
+    "revision": "6c801c315c654a1e9760",
+    "url": "/DegenChat/static/js/runtime-main.cdbb7b87.js"
+  },
+  {
+    "revision": "72e114df5a59bde11b2e4333182b90e8",
+    "url": "/DegenChat/static/media/logo_black_white.72e114df.png"
+  }
+]);
